9fdcf28c246e3270b101ac96b857813994ec0a04
Abel <abelsjar@gmail.com>
2018-02-16 03:17:06 -0800

    Lets see if nginx server will stand up

--- docker/recipes/default.rb
+++ docker/recipes/default.rb
@@ -3,6 +3,7 @@ when "ubuntu","debian"
   package "docker.io" do
     action :install
   end
+
 when 'centos','redhat','fedora','amazon'
   package "docker" do
     action :install
@@ -11,4 +12,22 @@ end
 
 service "docker" do
   action :start
+end
+
+# Pull latest image
+docker_image 'nginx' do
+  tag 'latest'
+  action :pull
+  notifies :redeploy, 'docker_container[my_nginx]'
+end
+
+# Run container mapping containers port 80 to the host's port 80
+docker_container 'my_nginx' do
+  repo 'nginx'
+  tag 'latest'
+  port '80:80'
+  host_name 'www'
+  domain_name 'computers.biz'
+  env 'FOO=bar'
+  volumes [ '/some/local/files/:/etc/nginx/conf.d' ]
 end
