edde2897ce74e4962707e374dc48b2ce5d8494b6
Abel <abelsjar@gmail.com>
2018-02-16 18:59:38 -0800

    removing first test cookbook

--- first_cookbook/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook:: first_cookbook
-# Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
-file "/etc/chef/first_cookbook/hello.txt" do
-  content "Yaya, we made it!\n"
-  action :create
-end
